fdb394934fb78397f43f91044635cb16959f8835
= <=>
2025-02-12 21:22:14 +0530

    done with entity class

--- src/main/java/com/restapi/demo/entity/Datasets.java
+++ src/main/java/com/restapi/demo/entity/Datasets.java
@@ -36,6 +36,96 @@ public class Datasets {
 
     @CreationTimestamp
     private LocalDateTime updatedAt;
+
+    public Datasets() {
+    }
+
+    public Datasets(String id, Map<String, Object> dataSchema, Map<String, Object> routeConfig, Status status, String createdBy, String updatedBy) {
+        this.id = id;
+        this.dataSchema = dataSchema;
+        this.routeConfig = routeConfig;
+        this.status = status;
+        this.createdBy = createdBy;
+        this.updatedBy = updatedBy;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, Object> getDataSchema() {
+        return dataSchema;
+    }
+
+    public void setDataSchema(Map<String, Object> dataSchema) {
+        this.dataSchema = dataSchema;
+    }
+
+    public Map<String, Object> getRouteConfig() {
+        return routeConfig;
+    }
+
+    public void setRouteConfig(Map<String, Object> routeConfig) {
+        this.routeConfig = routeConfig;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Datasets{" +
+                "id='" + id + '\'' +
+                ", dataSchema=" + dataSchema +
+                ", routeConfig=" + routeConfig +
+                ", status=" + status +
+                ", createdBy='" + createdBy + '\'' +
+                ", updatedBy='" + updatedBy + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
 
 enum Status {
