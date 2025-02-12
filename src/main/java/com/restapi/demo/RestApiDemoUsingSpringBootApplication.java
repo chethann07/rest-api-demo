7292f89ff59edf9ce4613ddc1033e59e16cf5d32
= <=>
2025-02-12 12:28:53 +0530

    first commit

---
+++ src/main/java/com/restapi/demo/RestApiDemoUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDemoUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDemoUsingSpringBootApplication.class, args);
+	}
+
+}
